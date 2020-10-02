6398b58ef501899c43df59a328b96fbd43e190ab
renal.nugraha@gmail.com <renal.nugraha00@gmail.com>
2020-10-02 23:02:23 +0700

    update file

--- main.js
+++ main.js
@@ -11,3 +11,15 @@ const closeModal = () => {
     // kita atur styling element modalnya
     modal.style.display = 'none'
 }
+
+// kita ambil dulu element yang diinginkan
+let showcase = document.getElementById('wrapper_showcase')
+// kita lanjut elementnya dengan sintax HTML
+// showcase.innerHTML += `
+// <img src="https://picsum.photos/seed/678/900" alt="gambar no 678" />
+// `
+for (var i = 0; i < 36; i++) {
+    showcase.innerHTML += `
+     <img src='https://picsum.photos/seed/${800 + i}/130' alt='gambar ke ${i}'/>
+    `
+}
